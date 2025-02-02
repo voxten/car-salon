d21204112d2d2c01ee02d8f4de9d52646ed7a5d7
voxten <voxterman@gmail.com>
2025-02-02 21:58:57 +0100

    Modified editing form

--- src/app/rent-a-car/[slug]/CarDetailsClient.js
+++ src/app/rent-a-car/[slug]/CarDetailsClient.js
@@ -1,8 +1,11 @@
 "use client";
 
 import { useState } from "react";
+import classes from "./page.module.css";
+import { useRouter } from "next/navigation";
 
 export default function CarDetailsClient({ car, slug }) {
+    const router = useRouter();
     const [editMode, setEditMode] = useState(false);
     const [formData, setFormData] = useState(car);
 
@@ -25,6 +28,7 @@ export default function CarDetailsClient({ car, slug }) {
             }
 
             setEditMode(false);
+            router.refresh();
         } catch (error) {
             console.error("Error updating car:", error);
         }
@@ -32,15 +36,119 @@ export default function CarDetailsClient({ car, slug }) {
 
     return (
         <div>
+            <div className={classes.headerText}>
+                {editMode ? (
+                    <input className={classes.title} name="name" value={formData.name} onChange={handleChange} />
+                ) : (
+                    <h1 className={classes.title}>{car.name}</h1>
+                )}
+                {editMode ? (
+                    <p className={classes.carInfo}><strong>Brand: </strong>
+                        <input className={classes.carInfo} name="brand" value={formData.brand} onChange={handleChange} />
+                    </p>
+                ) : (
+                    <p className={classes.carInfo}><strong>Brand:</strong> {car.brand}</p>
+                )}
+                {editMode ? (
+                    <p className={classes.carInfo}><strong>Model:</strong>
+                        <input className={classes.carInfo} name="model" value={formData.model} onChange={handleChange} />
+                    </p>
+                ) : (
+                    <p className={classes.carInfo}><strong>Model:</strong> {car.model} ({car.version})</p>
+                )}
+                {editMode ? (
+                    <p className={classes.carInfo}><strong>Version:</strong>
+                        <input className={classes.carInfo} name="version" value={formData.version} onChange={handleChange} />
+                    </p>
+                ) : (
+                    <p className={classes.carInfo}><strong>Version:</strong> {car.version}</p>
+                )}
+                {editMode ? (
+                    <p className={classes.carInfo}><strong>Color:</strong>
+                        <input className={classes.carInfo} name="color" value={formData.color} onChange={handleChange} />
+                    </p>
+                ) : (
+                    <p className={classes.carInfo}><strong>Color:</strong> {car.color}</p>
+                )}
+                {editMode ? (
+                    <p className={classes.carInfo}><strong>Price per day:</strong> $
+                        <input className={classes.carInfo} name="price_per_day" type="number" value={formData.price_per_day} onChange={handleChange} />
+                    </p>
+                ) : (
+                    <p className={classes.carInfo}><strong>Price per day:</strong> ${car.price_per_day}</p>
+                )}
+                {editMode ? (
+                    <p className={classes.carInfo}><strong>Fuel Type:</strong>
+                        <input className={classes.carInfo} name="fuel_type" value={formData.fuel_type} onChange={handleChange} />
+                    </p>
+                ) : (
+                    <p className={classes.carInfo}><strong>Fuel Type:</strong> {car.fuel_type} ({car.fuel_usage} L/100km)</p>
+                )}
+                {editMode ? (
+                    <p className={classes.carInfo}><strong>Fuel Usage:</strong>
+                        <input className={classes.carInfo} name="fuel_usage" type="number" value={formData.fuel_usage} onChange={handleChange} />L/100km
+                    </p>
+                ) : (
+                    <p className={classes.carInfo}><strong>Fuel Usage:</strong> {car.fuel_usage} L/100km</p>
+                )}
+                {editMode ? (
+                    <p className={classes.carInfo}><strong>Engine:</strong>
+                        <input className={classes.carInfo} name="engine_name" value={formData.engine_name} onChange={handleChange} />
+                    </p>
+                ) : (
+                    <p className={classes.carInfo}><strong>Engine:</strong> {car.engine_name}</p>
+                )}
+                {editMode ? (
+                    <p className={classes.carInfo}><strong>Power:</strong>
+                        <input className={classes.carInfo} name="power" type="number" value={formData.power} onChange={handleChange} />HP
+                    </p>
+                ) : (
+                    <p className={classes.carInfo}><strong>Power:</strong> {car.power} HP</p>
+                )}
+                {editMode ? (
+                    <p className={classes.carInfo}><strong>Acceleration:</strong>
+                        <input className={classes.carInfo} name="acceleration" type="number" value={formData.acceleration} onChange={handleChange} />sec (0-100 km/h)
+                    </p>
+                ) : (
+                    <p className={classes.carInfo}><strong>Acceleration:</strong> {car.acceleration} sec (0-100 km/h)</p>
+                )}
+                {editMode ? (
+                    <p className={classes.carInfo}><strong>Max Speed:</strong>
+                        <input className={classes.carInfo} name="max_speed" type="number" value={formData.max_speed} onChange={handleChange} />km/h
+                    </p>
+                ) : (
+                    <p className={classes.carInfo}><strong>Max Speed:</strong> {car.max_speed} km/h</p>
+                )}
+                {editMode ? (
+                    <p className={classes.carInfo}><strong>Gearbox:</strong>
+                        <input className={classes.carInfo} name="gearbox_type" value={formData.gearbox_type} onChange={handleChange} />
+                    </p>
+                ) : (
+                    <p className={classes.carInfo}><strong>Gearbox:</strong> {car.gearbox_type}</p>
+                )}
+                {editMode ? (
+                    <p className={classes.carInfo}><strong>Body Type:</strong>
+                        <input className={classes.carInfo} name="body_type" value={formData.body_type} onChange={handleChange} />
+                    </p>
+                ) : (
+                    <p className={classes.carInfo}><strong>Body Type:</strong> {car.body_type}</p>
+                )}
+                {editMode ? (
+                    <p className={classes.carInfo}><strong>Production Year:</strong>
+                        <input className={classes.carInfo} name="production_year" type="number" value={formData.production_year} onChange={handleChange} />
+                    </p>
+                ) : (
+                    <p className={classes.carInfo}><strong>Production Year:</strong> {car.production_year}</p>
+                )}
+            </div>
             {editMode ? (
-                <input name="name" value={formData.name} onChange={handleChange} />
+                <div className={classes.actions}>
+                    <button onClick={handleSubmit}><a>Submit</a></button>
+                </div>
             ) : (
-                <h1>{car.name}</h1>
-            )}
-            {editMode ? (
-                <button onClick={handleSubmit}>Submit</button>
-            ) : (
-                <button onClick={handleEditClick}>Edit</button>
+                <div className={classes.actions}>
+                    <button onClick={handleEditClick}><a>Edit</a></button>
+                </div>
             )}
         </div>
     );
--- src/app/rent-a-car/[slug]/page.js
+++ src/app/rent-a-car/[slug]/page.js
@@ -1,7 +1,7 @@
 "use server";
 import RentalSlider from '@/components/rental-slider';
 import classes from "./page.module.css";
-import { getCar, updateCar } from "@/app/lib/cars";
+import { getCar } from "@/app/lib/cars";
 import { notFound } from "next/navigation";
 import CarDetailsClient from './CarDetailsClient'; // Create a separate client component
 
@@ -20,19 +20,7 @@ export default async function CarDetailsPage({ params }) {
                     <img src={car.image} alt={car.name} className={classes.image} />
                 </div>
                 <div className={classes.headerText}>
-                    <h1 className={classes.title}>{car.name}</h1>
-                    <p className={classes.carInfo}><strong>Brand:</strong> {car.brand}</p>
-                    <p className={classes.carInfo}><strong>Model:</strong> {car.model} ({car.version})</p>
-                    <p className={classes.carInfo}><strong>Color:</strong> {car.color}</p>
-                    <p className={classes.carInfo}><strong>Price per day:</strong> ${car.price_per_day}</p>
-                    <p className={classes.carInfo}><strong>Fuel Type:</strong> {car.fuel_type} ({car.fuel_usage} L/100km)</p>
-                    <p className={classes.carInfo}><strong>Engine:</strong> {car.engine_name}</p>
-                    <p className={classes.carInfo}><strong>Power:</strong> {car.power} HP</p>
-                    <p className={classes.carInfo}><strong>Acceleration:</strong> {car.acceleration} sec (0-100 km/h)</p>
-                    <p className={classes.carInfo}><strong>Max Speed:</strong> {car.max_speed} km/h</p>
-                    <p className={classes.carInfo}><strong>Gearbox:</strong> {car.gearbox_type}</p>
-                    <p className={classes.carInfo}><strong>Body Type:</strong> {car.body_type}</p>
-                    <p className={classes.carInfo}><strong>Production Year:</strong> {car.production_year}</p>
+                    <CarDetailsClient car={car} slug={slug} />
                 </div>
             </header>
             <main className={classes.headerText}>
@@ -41,7 +29,7 @@ export default async function CarDetailsPage({ params }) {
                     <p className={classes.carInfo}>{car.information}</p>
                 </section>
             </main>
-            <CarDetailsClient car={car} slug={slug} />
+
             <div className={classes.rental}>
                 <RentalSlider pricePerDay={car.price_per_day} />
             </div>
